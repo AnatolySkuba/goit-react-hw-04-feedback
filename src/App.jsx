796b5b243e78f69f61a3191dcfd2007d938a2411
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-06-10 20:34:51 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,13 @@ export default function App() {
     const element = evt.target.innerText.toLowerCase();
     switch (element) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevState => prevState + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevState => prevState + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevState => prevState + 1);
         break;
 
       default:
